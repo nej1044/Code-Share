8cb5c9f9c10e1af7843f4921f5e389f8e7e165fb
nej1044 <nej1044@naver.com>
2022-09-26 21:35:49 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import Editor from "@monaco-editor/react";
+import {useCallback, useEffect, useRef,useState} from 'react';
+import firebase from "firebase";
+import firebaseConfig from './fireabaseConfig'
+import {fromMonaco} from '@hackerrank/firepad';
 
 function App() {
+
+  const editorRef = useRef(null);
+  const [editorLoaded,setEditorLoaded] = useState(false);
+  const [value, setValue] = useState('')
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+    setEditorLoaded(true);
+  }
+
+  const onChange = useCallback((value) => {
+    setValue(value)
+  }, []);
+
+  const InitFireBase = () => {
+    // Get Firebase Database reference.
+    let firepadRef = firebase.database().ref();
+
+    const urlparams = new URLSearchParams(window.location.search);
+    const roomId = urlparams.get("id");
+
+    if (roomId) {
+      firepadRef = firepadRef.child(roomId);
+    } else {
+      firepadRef = firepadRef.push();
+      window.history.replaceState(null, "Share Code", "?id=" + firepadRef.key);
+    }
+
+    return firepadRef;
+  };
+
+  useEffect(() => {
+    if(!firebase.apps.length){
+      // Make sure initialization happens only once
+      firebase.initializeApp(firebaseConfig); 
+    } 
+    else{
+      firebase.app();
+    }
+  }, []);
+
+  useEffect(() => {
+    if(!editorLoaded){
+      // If editor is not loaded return
+      return;
+    }
+
+    let dbRef = InitFireBase();
+    const editor = editorRef.current;
+    console.log(editor)
+     editor.onDidAttemptReadOnlyEdit(() => {
+    });
+
+
+    // const dbRef = firebase.database().ref().child(`${rooms[0]}`); // Can be anything in param, use unique string for unique code session
+
+    fromMonaco(dbRef,editorRef.current);
+
+    // const name = prompt("Enter your Name :"); // Name to highlight who is editing where in the code
+    // if(name){
+    //     firepad.setUserName(name);
+    // }
+
+  },[editorLoaded]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Editor
+       height="90vh"
+       defaultLanguage="javascript"
+       theme="vs-dark"
+       defaultValue= "// Welcome to My Editor"
+       onMount={handleEditorDidMount}
+       onChange={onChange}
+       options={{
+         readOnly: false,
+         accessibilitySupport: 'on'
+       }}
+     />
     </div>
   );
 }
---
+++ src/fireabaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDuAn4Q09B5HpX4mu-M5EeQZ1IeupXNtYo",
+    authDomain: "firepad-f3dd3.firebaseapp.com",
+    databaseURL: "https://firepad-f3dd3-default-rtdb.firebaseio.com",
+    projectId: "firepad-f3dd3",
+    storageBucket: "firepad-f3dd3.appspot.com",
+    messagingSenderId: "835665460266",
+    appId: "1:835665460266:web:300e5acb420a8b4f635753",
+    measurementId: "G-2E5QXLBS5Y"
+}
+
+export default firebaseConfig;
